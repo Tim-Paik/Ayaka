0b38cfb27e669347d1f17a9a84cf1ecd1e99a0dd
Tim-Paik <Tim-Paik@users.noreply.github.com>
2023-04-27 13:14:44 +0000

    deploy: e2d4b962bb40bfb81ad1d66c06f97f4392a26c82

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,8 +1,9 @@
 var sourcesIndex = JSON.parse('{\
+"ayaka_bindings":["",[],["fs.rs","lib.rs","logger.rs"]],\
+"ayaka_bindings_impl":["",[],["lib.rs"]],\
 "ayaka_bindings_types":["",[],["config.rs","fs.rs","lib.rs","logger.rs","plugin.rs"]],\
 "ayaka_model":["",[],["lib.rs","settings.rs","view_model.rs"]],\
 "ayaka_plugin":["",[],["lib.rs"]],\
-"ayaka_plugin_nop":["",[],["lib.rs"]],\
 "ayaka_plugin_wasmer":["",[],["lib.rs"]],\
 "ayaka_plugin_wasmi":["",[],["lib.rs"]],\
 "ayaka_plugin_wasmtime":["",[],["lib.rs"]],\
